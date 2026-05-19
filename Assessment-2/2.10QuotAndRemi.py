9046cd9f3c89f9289fa6a691c1655fe5763e93d2
Sem1505 <sempatel1505@gmail.com>
2026-05-19 16:30:11 +0530

    Divide two numbers and print the quotient and remainder separately

---
+++ Assessment-2/2.10QuotAndRemi.py
@@ -0,0 +1,8 @@
+x = float(input("Enter a Dividend: "))
+y = float(input("Enter a Divisor: "))
+
+Quotient = x / y
+Remainder = x % y
+
+print("Quotient is:", int(Quotient))
+print("Remainder is:", Remainder)
