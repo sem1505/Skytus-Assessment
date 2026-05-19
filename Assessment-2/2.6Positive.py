c1d9d92c5ebbf006d244be1bc8e65cf3b66d6fbc
Sem1505 <sempatel1505@gmail.com>
2026-05-19 14:10:30 +0530

    Take two numbers and print True if both are positive, else False.

---
+++ Assessment-2/2.6Positive.py
@@ -0,0 +1,7 @@
+num1=input("Enter First Number:" )
+num2=input("Enter Second Number:" )
+
+if int(num1)>0 and int(num2)>0:
+    print("Both numbers are positive.")
+else:
+    print("Both numbers are not positive.")
