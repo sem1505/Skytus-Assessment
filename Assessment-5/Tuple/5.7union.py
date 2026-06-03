3b3873bc2c3b5c6673aeff8bfe80a42eaa637cd3
Sem1505 <sempatel1505@gmail.com>
2026-06-03 12:40:04 +0530

    Find union of two sets

--- Assessment-5/Tuple/5.7union.py
+++ Assessment-5/Tuple/5.7union.py
@@ -1,4 +1,4 @@
-tuple1 = {"apple", "banana", "orange"}
-tuple2 = {"banana", "orange", "grape", "kiwi"}
-new_tuple = tuple1.union(tuple2)
-print(new_tuple)
+set1 = {"apple", "banana", "orange"}
+set2 = {"banana", "orange", "grape", "kiwi"}
+new_set = set1.union(set2)
+print(new_set)
