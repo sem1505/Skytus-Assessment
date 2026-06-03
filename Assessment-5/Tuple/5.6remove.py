b608662d408eb29c12b7750428771e0812f80b31
Sem1505 <sempatel1505@gmail.com>
2026-06-03 11:56:35 +0530

    Remove an element from a set

--- Assessment-5/Tuple/5.6remove.py
+++ Assessment-5/Tuple/5.6remove.py
@@ -1,7 +1,3 @@
-tuple1 = ("apple", "banana", "cherry", "orange", "grape")
-
-list_tuple = list(tuple1)
-list_tuple.remove("banana")
-
-
-print(tuple(list_tuple))
+set1 = {"apple", "banana", "cherry", "orange", "grape"}
+set1.remove("banana")
+print(set1)
